b90d5a87bb15ee22f0691bc3ca2ee529655a574c
MicoPham <michaelxpham@gmail.com>
2021-06-02 22:24:09 -0700

    Add simple UI with buttons using DOM methods

--- scripts/main.js
+++ scripts/main.js
@@ -10,43 +10,102 @@ function computerPlay() {
 let playerSelection;
 let computerSelection;
 
-function playRound(playerSelection, computerSelection) {
-  playerSelection = playerPlay();
-  computerSelection = computerPlay();
-  if ((playerSelection === 'rock') && (computerSelection === 0)) {
-    console.log('tie');
-    return t++;
-  } else if ((playerSelection === 'rock') && (computerSelection === 1)) {
-    console.log('player lose');
-    return l++;
-  } else if ((playerSelection === 'rock') && (computerSelection === 2)) {
-    console.log('player win');
-    return w++;
-  } else if ((playerSelection === 'paper') && (computerSelection === 0)) {
-    console.log('player win');
-    return w++;
-  } else if ((playerSelection === 'paper') && (computerSelection === 1)) {
-    console.log('tie');
-    return t++;
-  } else if ((playerSelection === 'paper') && (computerSelection === 2)) {
-    console.log('player lose');
-    return l++;
-  } else if ((playerSelection === 'scissors') && (computerSelection === 0)) {
-    console.log('player lose');
-    return l++;
-  } else if ((playerSelection === 'scissors') && (computerSelection === 1)) {
-    console.log('player win');
-    return w++;
-  } else if ((playerSelection === 'scissors') && (computerSelection === 2)) {
-    console.log('tie');
-    return t++;
-  }
-}
-
 let w = 0,
     l = 0,
     t = 0;
 
+function resetScores () {
+  w = 0;
+  l = 0;
+  t = 0;
+}
+
+function checkWinner() {
+    if (w >= 5) {
+    divDisplay.textContent = `player wins game with ${w} win(s), ${l} lose(s), ${t} tie(s).`;
+    container.appendChild(divDisplay);
+    resetScores()
+    return;
+  }
+  
+  if (l >= 5) {
+    divDisplay.textContent = `player loses game with ${w} win(s), ${l} lose(s), ${t} tie(s).`;
+    container.appendChild(divDisplay);
+    resetScores()
+    return;
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+ // playerSelection = playerPlay();
+  computerSelection = computerPlay();
+
+  if ((playerSelection === 'rock') && (computerSelection === 0)) {
+    divResults.textContent = 'tie';
+    t++;
+    divDisplay.textContent = `Player: ${w} Computer: ${l} Tie: ${t}`;
+    checkWinner()
+    return;
+  } else if ((playerSelection === 'rock') && (computerSelection === 1)) {
+    divResults.textContent = 'player lose';
+    container.appendChild(divResults);
+    l++;
+    divDisplay.textContent = `Player: ${w} Computer: ${l} Tie: ${t}`;
+    checkWinner()
+    return;
+  } else if ((playerSelection === 'rock') && (computerSelection === 2)) {
+    divResults.textContent = 'player win';
+    container.appendChild(divResults);
+    w++;
+    divDisplay.textContent = `Player: ${w} Computer: ${l} Tie: ${t}`;
+    checkWinner()
+    return;
+  } else if ((playerSelection === 'paper') && (computerSelection === 0)) {
+    divResults.textContent = 'player win';
+    container.appendChild(divResults);
+    w++;
+    divDisplay.textContent = `Player: ${w} Computer: ${l} Tie: ${t}`;
+    checkWinner()
+    return;
+  } else if ((playerSelection === 'paper') && (computerSelection === 1)) {
+    divResults.textContent = 'tie';
+    container.appendChild(divResults);
+    t++;
+    divDisplay.textContent = `Player: ${w} Computer: ${l} Tie: ${t}`;
+    checkWinner()
+    return;
+  } else if ((playerSelection === 'paper') && (computerSelection === 2)) {
+    divResults.textContent = 'player lose';
+    container.appendChild(divResults);
+    l++;
+    divDisplay.textContent = `Player: ${w} Computer: ${l} Tie: ${t}`;
+    checkWinner()
+    return;
+  } else if ((playerSelection === 'scissors') && (computerSelection === 0)) {
+    divResults.textContent = 'player lose';
+    container.appendChild(divResults);
+    l++;
+    divDisplay.textContent = `Player: ${w} Computer: ${l} Tie: ${t}`;
+    checkWinner()
+    return;
+  } else if ((playerSelection === 'scissors') && (computerSelection === 1)) {
+    divResults.textContent = 'player win';
+    container.appendChild(divResults);
+    w++;
+    divDisplay.textContent = `Player: ${w} Computer: ${l} Tie: ${t}`;
+    checkWinner()
+    return;
+  } else if ((playerSelection === 'scissors') && (computerSelection === 2)) {
+    divResults.textContent = 'tie';
+    container.appendChild(divResults);
+    t++;
+    divDisplay.textContent = `Player: ${w} Computer: ${l} Tie: ${t}`;
+    checkWinner()
+    return;
+  }
+}
+
+/*
 function game() {  
   playRound(playerSelection, computerSelection);
   playRound(playerSelection, computerSelection);
@@ -61,5 +120,30 @@ function game() {
       console.log(`game tie with ${w} win(s), ${l} lose(s), ${t} tie(s).`);
   }
 }
+*/
 
-game();
+const container = document.querySelector('#container');
+
+const btnRock = document.createElement('button');
+btnRock.classList.add('rock');
+btnRock.textContent = 'Rock';
+btnRock.addEventListener('click', () => playRound('rock'));
+container.appendChild(btnRock);
+
+const btnPaper = document.createElement('button');
+btnPaper.classList.add('paper');
+btnPaper.textContent = 'Paper';
+btnPaper.addEventListener('click', () => playRound('paper'));
+container.appendChild(btnPaper);
+
+const btnScissor = document.createElement('button');
+btnScissor.classList.add('scissor');
+btnScissor.textContent = 'Scissor';
+btnScissor.addEventListener('click', () => playRound('scissors'));
+container.appendChild(btnScissor);
+
+const divResults = document.createElement('div');
+
+const divDisplay = document.createElement('div');
+divDisplay.classList.add('display');
+container.appendChild(divDisplay);
